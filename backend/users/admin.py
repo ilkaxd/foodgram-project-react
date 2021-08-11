a66956ad94583736b835a5c898bd790751022764
ilka.xd <ilka.xd@yandex.ru>
2021-08-11 10:13:47 +0300

    change folder

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,70 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.models import Group
+# from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+# User = get_user_model()
+
+# @admin.register(User)
+# class CustomUserAdmin(UserAdmin):
+#     list_display = (
+#         'pk',
+#         'email',
+#         'username',
+#         'first_name', 'last_name',
+#         'is_staff',
+#     )
+#     search_fields = ('username', 'email', 'first_name', 'last_name')
+#     list_filter = ('email', 'username')
+#     readonly_fields = ('date_joined', 'last_login')
+
+#     fieldsets = (
+#         (None, {'fields': ('username', 'password')}),
+#         ('Personal info', {'fields': ('email', 'first_name', 'last_name')}),
+#         ('Permissions', {
+#             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')
+#         }),
+#         ('Important dates', {'fields': ('last_login', 'date_joined')})
+#     )
+
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': (
+#                 'email',
+#                 'username',
+#                 'first_name', 'last_name',
+#                 'password1', 'password2'
+#             ),
+#         }),
+#     )
+#     search_fields = ('username',)
+#     ordering = ('username',)
+
+#     def get_form(self, request, obj=None, **kwargs):
+#         form = super().get_form(request, obj=obj, **kwargs)
+#         is_superuser = request.user.is_superuser
+#         disabled_fields = set()
+#         if not is_superuser:
+#             disabled_fields.add([
+#                 'username',
+#                 'is_superuser',
+#                 'user_permissions',
+#             ])
+#             if (
+#                 obj is not None
+#                 and
+#                 obj == request.user
+#             ):
+#                 disabled_fields.add([
+#                     'is_active',
+#                     'is_staff',
+#                     'is_superuser',
+#                     'groups',
+#                     'user_permissions',
+#                 ])
+#         for field in disabled_fields:
+#             if field in form.base_fields:
+#                 form.base_fields[field].disabled = True
+#         return form
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# class CustomUser(AbstractUser):
+#     first_name = models.CharField(
+#         verbose_name='Имя',
+#         max_length=50,
+#         blank=False,
+#         help_text='Required 50 characters of less'
+#     )
+
+#     last_name = models.CharField(
+#         verbose_name='Фамилия',
+#         max_length=100,
+#         blank=False,
+#         help_text='Required 10 characters of less'
+#     )
+
+#     email = models.EmailField(
+#         verbose_name='Email',
+#         unique=True,
+#         blank=False,
+#         help_text='Required email'
+#     )
+
+#     class Meta:
+#         app_label = 'users'
+#         verbose_name = 'Пользователь'
+#         verbose_name_plural = 'Пользователи'
