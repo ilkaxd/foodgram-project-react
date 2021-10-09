337eb4721e621cb57b78a3469a062b859c1fb4c9
ilka.xd <ilka.xd@yandex.ru>
2021-10-09 11:56:40 +0300

    fix

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,9 +1,13 @@
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 from decouple import config
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTING_MODULE'))
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 
 application = get_wsgi_application()
